0008bd88013082eb259d8e25d612e0d795757b39
lanomw <740790513@qq.com>
2022-12-30 23:15:24 +0800

    feat：[青书学堂]作业答案自动填入

--- src/青书学堂视频挂机/README.md
+++ src/青书学堂视频挂机/README.md
@@ -1,13 +1,19 @@
 # 青书学堂视频挂机
 
+> 该脚本根据我自己的需要进行开发，不能覆盖到所有场景。有问题请到git仓库提问，能即使收到消息
+
 ## 视频自动播放
+
 > 进入播放页面后会进行监听视频是否播放完成。播放完成则自动播放下一个视频
 
 ![img.png](https://raw.githubusercontent.com/lanomw/TampermonkeyScript/main/src/%E9%9D%92%E4%B9%A6%E5%AD%A6%E5%A0%82%E8%A7%86%E9%A2%91%E6%8C%82%E6%9C%BA/img.png)
 
 ## 作业答案
-> 不支持自动答题，打开答题页面同时会打开答案页面，对着填就行
-> 
+
+> 支持作业答案自动填入，大致确认下没问题即可交卷。如果自动答题发生异常会自动打答案页面，对着填就行
+
+![img_2.png](https://raw.githubusercontent.com/lanomw/TampermonkeyScript/main/src/%E9%9D%92%E4%B9%A6%E5%AD%A6%E5%A0%82%E8%A7%86%E9%A2%91%E6%8C%82%E6%9C%BA/img_2.png)
+
 > chrome可能会自动拦截窗口打开，需要设置为允许
 
 ![img_1.png](https://raw.githubusercontent.com/lanomw/TampermonkeyScript/main/src/%E9%9D%92%E4%B9%A6%E5%AD%A6%E5%A0%82%E8%A7%86%E9%A2%91%E6%8C%82%E6%9C%BA/img_1.png) 
--- src/青书学堂视频挂机/青书学堂视频挂机.js
+++ src/青书学堂视频挂机/青书学堂视频挂机.js
@@ -1,71 +1,32 @@
 // ==UserScript==
 // @name         青书学堂视频挂机
 // @namespace    https://github.com/lanomw
-// @version      0.3
-// @description  青书学堂视频自动静音播放，解放双手。支持自动播放视频、作业答案查看
+// @version      1.0
+// @description  青书学堂视频自动静音播放，解放双手。支持自动播放视频、作业答案自动填入
 // @author       lanomw
 // @match        *://*.qingshuxuetang.com/*
 // @icon         https://degree.qingshuxuetang.com/resources/default/images/favicon.ico
+// @require      https://unpkg.com/pxmu@1.1.0/dist/web/pxmu.min.js
 // @grant        none
 // @license      MIT
 // ==/UserScript==
 
 (function () {
-    'use strict';
+    'use strict'
 
     setTimeout(function () {
-        autoPlayVideo()
-        showAnswer()
+        // 看网课
+        if (location.href.indexOf('cw_nodeId') !== -1) {
+            autoPlayVideo()
+        }
+
+        // 做作业
+        if (location.href.indexOf('ExercisePaper') !== -1) {
+            autoFillAnswer()
+        }
     }, 3000)
 
 })();
-
-// 自动播放视频
-function autoPlayVideo() {
-    // 非播放页面阻断执行
-    if (location.href.indexOf('cw_nodeId') === -1) {
-        return
-    }
-
-    const urlSearch = UrlSearch()
-
-    // 当前id
-    const cw_nodeId = `courseware-${urlSearch.cw_nodeId}`
-    // 课程列表
-    const lessonList = document.getElementById('lessonList').children
-    // 下一个课程
-    const next_cw_nodeId = getNextLession(lessonList, cw_nodeId)
-
-
-    const video = document.getElementsByTagName("video")[0]
-    // 静音、倍速
-    video.muted = true
-    // 设置倍速播放 支持以下速率: [2, 1.5, 1.2, 0.5]
-    video.playbackRate = 2
-    video.play()
-
-    // 视频播放结束则跳转
-    console.log(cw_nodeId, '---> to -->', next_cw_nodeId)
-    video.addEventListener("ended", function () {
-        if (next_cw_nodeId) {
-            const lession = document.getElementById(next_cw_nodeId)
-            lession && lession.click()
-        }
-    })
-}
-
-// 作业答案
-function showAnswer() {
-    // 非作业答题页面阻断执行
-    if (location.href.indexOf('ExercisePaper') === -1) {
-        return
-    }
-
-    const url = location.href.replace('ExercisePaper', 'ViewQuiz')
-
-    // 窗口可能会被浏览器拦截。需要允许
-    window.open(url, '_blank')
-}
 
 // url参数转换为对象
 function UrlSearch() {
@@ -80,11 +41,38 @@ function UrlSearch() {
     return params
 }
 
+// 自动播放视频
+function autoPlayVideo() {
+    const urlSearch = UrlSearch()
+
+    // 当前id
+    const cw_nodeId = `
+courseware -${urlSearch.cw_nodeId}`
+    // 课程列表
+    const lessonList = document.getElementById('lessonList').children
+    // 下一个课程
+    const next_cw_nodeId = getNextLession(lessonList, cw_nodeId)
+
+    const video = document.getElementsByTagName("video")[0]
+    // 静音、倍速
+    video.muted = true
+    // 设置倍速播放 支持以下速率: [2, 1.5, 1.2, 0.5]
+    video.playbackRate = 2
+    video.play()
+
+    // 视频播放结束则跳转
+    video.addEventListener("ended", function () {
+        if (next_cw_nodeId) {
+            const lession = document.getElementById(next_cw_nodeId)
+            lession && lession.click()
+        }
+    })
+}
+
 // 根据当前课程id递归获取下一个课程
 function getNextLession(list, cw_nodeId) {
     let nodeId = ''
     let isMatch = false
-
 
     function findLession(list) {
         for (let i = 0; i < list.length; i++) {
@@ -100,7 +88,7 @@ function getNextLession(list, cw_nodeId) {
                 // 已匹配到当前课程。获取下一个课程
                 if (children[0].id === cw_nodeId) {
                     isMatch = true
-                    continue;
+                    continue
                 }
             } else {
                 // 递归
@@ -117,4 +105,53 @@ function getNextLession(list, cw_nodeId) {
     findLession(list)
 
     return nodeId
+}
+
+// 答案自动填入
+function autoFillAnswer() {
+    var urlSearch = UrlSearch()
+
+    fetch(`
+https://degree.qingshuxuetang.com/xnsy/Student/DetailData?_t=${new Date().getMilliseconds()}&quizId=${urlSearch.quizId}`, {
+        method: 'GET',
+        headers: {
+            Host: 'degree.qingshuxuetang.com',
+            Cookie: Object.entries(Cookies.get()).map(([key, value]) => `${key}=${value}`).join('; '),
+            Referer: `https://degree.qingshuxuetang.com/xnsy/Student/ExercisePaper?courseId=${urlSearch.courseId}&quizId=${urlSearch.quizId}&teachPlanId=${urlSearch.teachPlanId}&periodId=${urlSearch.periodId}`,
+            'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': 'macOS',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-origin',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+            'X-Requested-With': 'XMLHttpRequest',
+        },
+    }).then(res => res.json()).then(res => {
+        var questions = res.data.paperDetail.questions
+
+        Object.values(questions).forEach(item => {
+            // 处理单选/多选 答案填入
+            for (let i = 0; i < item.solution.length; i++) {
+                $(`#${item.questionId}_${item.solution.charAt(i)}`).click()
+            }
+        })
+
+        pxmu.success({
+            msg: '答案已自动填入',
+            bg: '#4CC443',
+        })
+    }).catch(err => {
+        pxmu.fail({
+            msg: '答案已填入失败。请手动填入答案',
+            bg: 'red',
+        })
+
+        setTimeout(() => {
+            // 异常则直接打开答案查看页面。窗口可能会被浏览器拦截。需要允许
+            window.open(location.href.replace('ExercisePaper', 'ViewQuiz'), '_blank')
+        }, 1500)
+
+        console.error(err)
+    });
 }
